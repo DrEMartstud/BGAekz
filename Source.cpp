194884f99094c53ae79c5e8b9c60d8f0a284a6b9
dremartstud <dremartstud@gmail.com>
2020-02-23 21:22:35 +0200

    initial commit
    3 первых задания решено

---
+++ Source.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+
+
+using namespace std;
+void first() {
+//1)Создать пустой вектор типа string.Написать программу, которая записывает в вектор предложения и вывести содержимое вектора на экран.
+	vector <string> ourVector;
+	string s;
+	for (int i = 0; i < 5; i++) {
+		cout << i << ": ";
+		cin >> s;
+		ourVector.push_back(s);
+	}
+	vector<string>::iterator ukaz;
+	for (ukaz = ourVector.begin(); ukaz != ourVector.end(); ukaz++) cout << *ukaz << endl;
+}
+
+void second() {
+	//2) Создать пустой вектор типа int. Написать программу, которая записывает в вектор только положительные числа. 
+	//При вводе отрицательного числа заполнение вектора прекратить. 
+	//Вывести содержимое вектора на экран с помощью цикла, основанного на диапазоне.
+	vector <int> intVector;
+	int number;
+	cout << "Number > 0\n";
+	while ((cin >> number) && number > 0) intVector.push_back(number);
+	vector<int>::iterator znachenie;
+	for (znachenie = intVector.begin(); znachenie < intVector.end(); znachenie++) cout << *znachenie << endl;
+}
+void third() {
+//3) Создать пустой вектор типа double. 
+//Написать программу, которая записывает в вектор числа до тех пор, пока не введен ноль. 
+//Вывести на экран с помощью итератора только положительные элементы вектора. 
+
+	vector <double> doubleVector;
+	double number;
+	cout << "Number != 0\n";
+	while ((cin >> number) && number != 0) doubleVector.push_back(number);
+	vector<double>::iterator znachenie;
+	for (znachenie = doubleVector.begin(); znachenie < doubleVector.end(); znachenie++) if (*znachenie > 0) cout << *znachenie << endl;
+}
+int main() {
+	
+	return 0;
+}
